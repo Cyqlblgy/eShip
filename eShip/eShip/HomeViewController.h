a32ae3b7dab91bbfdbeb0ed3e239a6f1907190bf
Bin Lang <blang@roamdata.com>
2015-05-12 21:49:14 -0400

    HomeVC added
    HomeVC edited for four actions

---
+++ eShip/eShip/HomeViewController.h
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.h
+//  eShip
+//
+//  Created by Bin Lang on 5/10/15.
+//  Copyright (c) 2015 eShip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIView *trackingView;
+@property (weak, nonatomic) IBOutlet UIView *checkingView;
+@property (weak, nonatomic) IBOutlet UIView *shippingView;
+@property (weak, nonatomic) IBOutlet UIView *mapView;
+
+
+@end
---
+++ eShip/eShip/HomeViewController.m
@@ -0,0 +1,45 @@
+//
+//  HomeViewController.m
+//  eShip
+//
+//  Created by Bin Lang on 5/10/15.
+//  Copyright (c) 2015 eShip. All rights reserved.
+//
+
+#import "HomeViewController.h"
+#import "FindViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+@synthesize imageView,checkingView,shippingView,mapView,trackingView;
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    CGRect rect = imageView.frame;
+    CGRect screenFrame = [UIScreen mainScreen].bounds;
+    trackingView.frame = CGRectMake(0, rect.origin.y+rect.size.height, rect.size.width/2, (screenFrame.size.height-40-rect.origin.y-rect.size.height)/2);
+    checkingView.frame = CGRectMake(rect.size.width/2, rect.origin.y+rect.size.height, rect.size.width/2, (screenFrame.size.height-40-rect.origin.y-rect.size.height)/2);
+    shippingView.frame = CGRectMake(0, (screenFrame.size.height-40+ rect.origin.y+rect.size.height)/2, rect.size.width/2, (screenFrame.size.height-40-rect.origin.y-rect.size.height)/2);
+    mapView.frame = CGRectMake(rect.size.width/2, (screenFrame.size.height-40+ rect.origin.y+rect.size.height)/2, rect.size.width/2, (screenFrame.size.height-40-rect.origin.y-rect.size.height)/2);
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
